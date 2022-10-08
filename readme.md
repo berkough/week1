d47b95a2ce761fbef787bdee92d847438e45dc22
Mathew Kane <berkough@gmail.com>
2022-10-08 16:37:12 -0700

    cleaned up the code a bit, made output more friendly to read

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 #week1
 
 This coding assingment is designed to teach us the basics of Git and Github as well as how to use variables and operators in Javascript.
+Initially this script did absolutely nothing, just stored some values in to variables as examples of the basic types. I then added some basic functionality to output to the console and test working with different values.
--- variables.js
+++ variables.js
@@ -4,7 +4,7 @@ let firstName = 'Mathew';
 let lastName = 'Kane';
 
 //Concatenating strings in the console.
-console.log('The user\'s name is: ' + firstName + lastName);
+console.log('The user\'s name is: ' + firstName + " " + lastName);
 
 //Numbers, both whole and decimal.
 let num1 = 1;
@@ -24,12 +24,12 @@ let remainder = num2 % num1;
 console.log('This is the output from adding the variables num1 and num2 together: ' + addition);
 console.log('This is the output from subtracting num2 from num1: ' + subtraction);
 console.log('This is the output from multiplying num1 and num2: ' + multiply);
-console.log('This is the output from dividing num2 by num1: ' + divide);
+console.log('This is the output from dividing num2 by num1: ' + divide.toFixed(2));
 console.log('This is the remainder of num2 divided by num2: ' + remainder);
 
 firstName = 'Matt';
 const constantOfPi = 3.14159265359;
-console.log('The user has shortened his first name to ' + firstName + ' and has declared the constant ' + constantOfPi);
+console.log('The user has shortened his first name to ' + firstName + ' and has declared the constant ' + constantOfPi.toFixed(2));
 
 let temperature = 76;
 let celcius = (temperature - 32) * 5/9;
@@ -39,4 +39,4 @@ console.log("The temperature is "+ temperature + "F " + "and " + celcius.toFixed
 let radius = 3;
 let area = constantOfPi * Math.pow(radius,2);
 
-console.log("The variable radius is currently set to 3, the area of the circle is " + area.toFixed(2));
+console.log("The variable radius is currently set to" + radius + " , the area of the circle is " + area.toFixed(2));
